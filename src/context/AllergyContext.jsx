7c8f1e01e60642ab896426c015a2e8e7747c797b
Dgames_Crew <ddan9926@gmail.com>
2026-04-06 16:34:16 -0500

    more allergies

--- src/context/AllergyContext.jsx
+++ src/context/AllergyContext.jsx
@@ -8,7 +8,7 @@ export function AllergyProvider({ children }) {
   // Allergies user has
   const [allergies, setAllergies] = useState(["Peanuts"]);
   // "DB" List of all allergies - TODO add more
-  const [allergiesData] = useState(["Tree Nuts", "Peanuts", "Dairy", "Shellfish", "Other"]);
+  const [allergiesData] = useState(["Tree Nuts", "Peanuts", "Dairy", "Shellfish", "Eggs", "Wheat", "Soybeans", "Sesame", "Fish"]);
   const [selectedAllergies, setSelectedAllergies] = useState([]);
   return (
     <AllergyContext.Provider value={{ allergies, setAllergies, allergiesData, selectedAllergies, setSelectedAllergies }}>
